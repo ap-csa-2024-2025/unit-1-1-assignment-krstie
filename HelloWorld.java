bb0e8c838dd9184af40bd211a7918fe51bba9beb
krstie <kristetran010308@gmail.com>
2024-09-03 11:21:15 -0700

    hi

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,21 @@ public class HelloWorld
 {
   public static void main(String[] args)
   {
-    System.out.print("Hello world!");
+    System.out.println("Hello world!");
+    System.out.println("Hello world!");
+
+    int myAge; 
+    myAge = 100; 
+
+    double price = 4.99; // set price to 4.99
+    String myName = "Kristie Tran"; 
+    int myNum = 10; 
+    double price2 = 5.99;
+    String friendName = "Emilia Nguyen";
+    boolean hi = true; 
+
+    System.out.println("Hello" + myName + "it's nice to meet you"); 
+
+
   }
 }
